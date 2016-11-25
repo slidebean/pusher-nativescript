5f32a4cabade9def26fbb39dd0a89a244b2e5fdb
Juan Delgadillo <j.mnuelh@gmail.com>
2016-11-25 17:35:59 -0600

    Capability to subscribe to public, private and presence channels has been added.

--- src/ios/pusher.ts
+++ src/ios/pusher.ts
@@ -1,11 +1,12 @@
-declare let NSObject, NSDictionary, PTPusherDelegate, PTPusher;
+declare let NSObject, NSDictionary, NSURL, PTPusherDelegate, PTPusherPresenceChannelDelegate, PTPusher;
 import { PusherOptions } from '../interfaces/PusherOptions';
 import { Observable } from 'rxjs/Rx';
 
 NSDictionary.prototype.toJSON = function () {
-  var result = {};
-  for ( var key of this ) {
-    result[key] = this.objectForKey(key);
+  let result = {};
+  for ( let index = 0; index < this.allKeys.count; index ++ ) {
+    let key = this.allKeys[index];
+    result[key] = this.allValues[index];
   }
   return result;
 };
@@ -15,14 +16,20 @@ export class Pusher {
   _options;
   _connectionPromiseResolve;
   _connectionPromiseReject;
+  _pusherDidSubscribeToChannelPromiseResolve;
+  _pusherDidSubscribeToChannelPromiseReject;
 
   constructor (appKey: String, options: PusherOptions = { encrypted: true }) {
+
+    if (typeof appKey === 'undefined') {
+      throw(new Error('pusher-nativescript package error: appKey parameter is required'))
+    }
 
     if (typeof appKey !== 'string') {
       throw(new Error('pusher-nativescript package error: appKey parameter must be a string'))
     }
 
-    if (typeof options !== 'object' && typeof options !== 'undefined') {
+    if (typeof options !== 'object') {
       throw(new Error('pusher-nativescript package error: options parameter must be an object'))
     }
 
@@ -53,6 +60,7 @@ export class Pusher {
 
       pusherConnectionWillAutomaticallyReconnectAfterDelay: (pusher, connection, delay) => {
         console.log('pusherConnectionWillAutomaticallyReconnectAfterDelay');
+        return true;
       },
 
       pusherWillAuthorizeChannelWithAuthOperation: (pusher, channel, operation) => {
@@ -61,6 +69,7 @@ export class Pusher {
 
       pusherDidSubscribeToChannel: (pusher, channel) => {
         console.log('pusherDidSubscribeToChannel');
+        this._pusherDidSubscribeToChannelPromiseResolve(channel);
       },
 
       pusherDidUnsubscribeFromChannel: (pusher, channel) => {
@@ -69,6 +78,8 @@ export class Pusher {
 
       pusherDidFailToSubscribeToChannelWithError: (pusher, channel, error) => {
         console.log('pusherDidFailToSubscribeToChannelWithError');
+        let subscriptionError = error.userInfo.objectForKey('NSUnderlyingError').userInfo.objectForKey('NSLocalizedDescription');
+        this._pusherDidSubscribeToChannelPromiseReject(subscriptionError);
       },
 
       pusherDidReceiveErrorEvent: (pusher, errorEvent) => {
@@ -80,8 +91,7 @@ export class Pusher {
 
     let delegate = Delegate.alloc().init();
 
-
-    if (options.cluster !== undefined) {
+    if (typeof options.cluster !== 'undefined') {
       if (typeof options.cluster !== 'string') {
         throw(new Error('pusher-nativescript package error: options.cluster property must be a string'));
       }
@@ -89,8 +99,15 @@ export class Pusher {
       this._pusher = PTPusher.pusherWithKeyDelegateEncryptedCluster(appKey, delegate, options.encrypted, options.cluster);
     } else {
       this._pusher = PTPusher.pusherWithKeyDelegateEncrypted(appKey, delegate, options.encrypted);
-    } 
+    }
 
+    if (typeof options.authorizer !== 'undefined') {
+      if (typeof options.authorizer !== 'string') {
+        throw(new Error('pusher-nativescript package error: options.authorizer property must be a string'));
+      }
+
+      this._pusher.authorizationURL = NSURL.URLWithString(options.authorizer);
+    }
 
   }
 
@@ -108,16 +125,77 @@ export class Pusher {
     this._pusher.disconnect();
   }
 
-  subscribe (channelName: String, eventName: String) {
-    return new Promise((resolve, reject) => {
+  subscribe (channelTypeAndName: String, eventName: String, listener: Function) {
+    let channelTypes = ['public', 'private', 'presence'];
+    let [publicType, privateType, presenceType] = channelTypes;
 
-      if (typeof channelName !== 'string') {
-        reject('channelName parameter must be a string');
+    return new Promise((resolve, reject) => {
+      let channel;
+
+      if (typeof channelTypeAndName === 'undefined') {
+        reject('The channelTypeAndName parameter is required');
       }
 
-      let channelType = channelName.split('-')[0];
+      if (typeof channelTypeAndName !== 'string') {
+        reject('The channelTypeAndName parameter must be a string');
+      }
 
+      if (typeof eventName === "undefined") {
+        reject('The eventName parameter is required');
+      }
+
+      if (typeof eventName !== "string") {
+        reject('The eventName parameter must be a string');
+      }
+
+      if (eventName.length === 0) {
+        reject('The eventName parameter can not be empty');
+      }
+
+      let [channelType, channelName] = channelTypeAndName.split('-');
+
+      if (channelTypes.indexOf(channelType) === -1) {
+        reject('The channelTypeAndName parameter must has the type of the channel');
+      }
+
+      if (typeof channelName === 'undefined' || channelName.length === 0) {
+        reject('The channelTypeAndName parameter must has the name of the channel');
+      }
+
+      if (typeof listener === 'undefined') {
+        reject('The listener parameter is required');
+      }
+
+      if (typeof listener !== 'function') {
+        reject('The listener parameter must be a function');
+      }
+
+      let pusherDidSubscribeToChannelPromise = new Promise((resolve, reject) => {
+        this._pusherDidSubscribeToChannelPromiseResolve = resolve;
+        this._pusherDidSubscribeToChannelPromiseReject = reject;
+      })
+
+      if (channelType === publicType) {
+        this._pusher.subscribeToChannelNamed(channelName);
+      }
+
+      if (channelType === privateType) {
+        this._pusher.subscribeToPrivateChannelNamed(channelName);
+      }
+
+      if (channelType === presenceType) {
+        this._pusher.subscribeToPresenceChannelNamed(channelName);
+      }
       
+      pusherDidSubscribeToChannelPromise.then((channel: Object) => {
+        channel.bindToEventNamedHandleWithBlock(eventName, channelEvent => {
+          let eventData = { channel: channelEvent.channel, eventName: channelEvent.name, data: channelEvent.data.toJSON() };
+          listener(eventData);
+        }) 
+        resolve();
+      }).catch((error) => {
+        reject(error);
+      });
 
     });
   }
