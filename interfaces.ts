f8a1422427e250e1f45c5fa5267b67400726e1ad
Juan Delgadillo <j.mnuelh@gmail.com>
2016-12-06 13:58:06 -0600

    A common file to it can be imported all interfaces

---
+++ interfaces.ts
@@ -0,0 +1,4 @@
+export * from './src/interfaces/PublicChannelEventListener';
+export * from './src/interfaces/PrivateChannelEventListener';
+export * from './src/interfaces/PresenceChannelEventListener';
+export * from './src/interfaces/PusherOptions';
