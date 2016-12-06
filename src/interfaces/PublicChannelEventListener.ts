04eec71d168ffceb87d9831bdd717b037154d4d3
Juan Delgadillo <j.mnuelh@gmail.com>
2016-12-06 12:32:08 -0600

    ChannelEventListener was renamed. Also It's been docummented.

---
+++ src/interfaces/PublicChannelEventListener.ts
@@ -0,0 +1,18 @@
+export interface PublicChannelEventListener {
+
+  /**
+   * Callback that is fired whenever an event is received.
+   *
+   * @param data An object with the event's information
+   */
+  onEvent (data: Object): void;
+
+  /**
+   * Callback that is fired when a subscription success acknowledgement
+   * message is received from Pusher after subscribing to the channel.
+   *
+   * @param channelName The name of the channel that was successfully subscribed.
+   */
+  onSubscriptionSucceeded (channelName: String): void;
+
+}
